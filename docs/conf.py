bef89497f0457b33e6a0da675a5797d5b074420f
DisizDream <disizdream@gmail.com>
2016-09-16 14:50:59 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'L1ght engine'
+copyright = u'2016, L1ght engine'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
